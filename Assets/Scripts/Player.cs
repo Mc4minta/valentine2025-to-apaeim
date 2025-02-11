6f7af5e60ea1dfc15f2e9f42e730ce2ba35f5480
Mc4minta <142011388+Mc4minta@users.noreply.github.com>
2025-02-12 02:20:37 +0700

    add walking animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,23 +7,36 @@ public class Player : MonoBehaviour
     public MovementJoystick movementJoystick;
     public float playerSpeed;
     private Rigidbody2D rb;
+    private Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
+        Vector2 movement = movementJoystick.joystickVec;
+            
         if(movementJoystick.joystickVec.y != 0)
         {
             rb.velocity = new Vector2(movementJoystick.joystickVec.x * playerSpeed,
                 movementJoystick.joystickVec.y * playerSpeed);
+
+            animator.SetBool("IsWalking", true);
+
+            float direction = movement.x > 0 ? 1f : -1f;
+
+            animator.SetFloat("Direction", direction);
+
         }
 
         else
         {
             rb.velocity = Vector2.zero;
+
+            animator.SetBool("IsWalking", false);
         }
     }
 
